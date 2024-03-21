34c5b8a4a058b78480117878513671f2be744eb4
garunAMT <21ME02022@iitbbs.ac.in>
2024-03-21 20:45:03 +0530

    designed UI and connected PostgreSQL database

---
+++ src/app/search/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation"
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const page = ({searchParams}: PageProps) => {
+
+  const query = searchParams.query
+  
+  if(Array.isArray(query) || !query) {
+    return redirect('/')
+  }
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
